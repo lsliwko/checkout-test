b344fc20f205e029b8eb3134bdfc1c2bf1e2088d
Leszek Sliwko <leszek.sliwko@concentra.co.uk>
2017-12-20 10:49:21 +0000

    updates

--- src/main/scala/com/checkout/CalculateTotalService.scala
+++ src/main/scala/com/checkout/CalculateTotalService.scala
@@ -3,31 +3,13 @@ package com.checkout
 
 object CalculateTotalService {
 
-  def calculateTotal(items : Iterable[Item]) : Double = {
-    //gives Map(Orange -> 2, Apple -> 3)
-    val itemsCount  = items.groupBy(identity).mapValues(_.size)
+  //promotions:
+  //apples 3 for 2
+  //oranges 5 for 3
 
-    itemsCount.map{case (item, count) => calulateTotalPerItemType(item, count)}.sum
-  }
 
-  private def calulateTotalPerItemType(item : Item, count : Int) : Double = {
-    item match {
-      case Apple() => ((count + 1)/2) * Apple().price       //buy one, get one free on Apples
-      case Orange() => ((count + 1)*2/3) * Orange().price   //3 for the price of 2
-    }
-  }
-
-  def parseItems(args : Iterable[String]) : Iterable[Item] = {
-    args.map{
-      case "apple" => Apple()
-      case "orange" => Orange()
-      case _ => throw new IllegalArgumentException
-    }
-  }
 
   def main(args : Array[String]): Unit = {
-    val items = parseItems(args)
-
-    println("Total=" + calculateTotal(items))
+    println("Total = " + calculateTotal(args))
   }
 }
--- src/test/scala/com/checkout/CalculateTotalServiceTest.scala
+++ src/test/scala/com/checkout/CalculateTotalServiceTest.scala
@@ -4,67 +4,8 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class CalculateTotalServiceTest extends FlatSpec with Matchers {
 
-  "Checkout" should "parse known items correctly" in {
-    CalculateTotalService.parseItems(Iterable("orange")) should be(Iterable(Orange()))
-
-    CalculateTotalService.parseItems(Iterable("apple")) should be(Iterable(Apple()))
-
-    CalculateTotalService.parseItems(Iterable("orange", "orange")) should be(Iterable(Orange(),Orange()))
-
-    CalculateTotalService.parseItems(Iterable("orange", "apple")) should be(Iterable(Orange(),Apple()))
+  "Checkout" should "do something correctly" in {
 
   }
 
-  it should "parse empty items list as empty" in {
-    CalculateTotalService.parseItems(Iterable()) should be(Iterable())
-  }
-
-  it should "throw error when parsing unknown item" in {
-
-    a[IllegalArgumentException] should be thrownBy {
-      CalculateTotalService.parseItems(Iterable("dummy"))
-    }
-
-    a[IllegalArgumentException] should be thrownBy {
-      CalculateTotalService.parseItems(Iterable("apple","dummy"))
-    }
-  }
-
-  it should "calculate items prices correctly" in {
-    CalculateTotalService.calculateTotal(Iterable(Apple())) should be(0.60)
-
-    CalculateTotalService.calculateTotal(Iterable(Orange())) should be(0.25)
-
-    CalculateTotalService.calculateTotal(Iterable(Orange(), Apple())) should be(0.85)
-
-    CalculateTotalService.calculateTotal(Iterable(Orange(), Orange(), Orange(), Apple(), Apple())) should be(1.10)
-  }
-
-  it should "parse empty empty items list as zero" in {
-    CalculateTotalService.calculateTotal(Iterable()) should be(0)
-  }
-
-  it should "calculate discounted Apples price correctly" in {
-    CalculateTotalService.calculateTotal(Iterable(Apple())) should be(0.60)
-
-    CalculateTotalService.calculateTotal(Iterable(Apple(), Apple())) should be(0.60)
-
-    CalculateTotalService.calculateTotal(Iterable(Apple(), Apple(), Apple())) should be(1.20)
-
-    CalculateTotalService.calculateTotal(Iterable(Apple(), Apple(), Apple(), Apple())) should be(1.20)
-  }
-
-  it should "calculate discounted Oranges price correctly" in {
-    CalculateTotalService.calculateTotal(Iterable(Orange())) should be(0.25)
-
-    CalculateTotalService.calculateTotal(Iterable(Orange(), Orange())) should be(0.50)
-
-    CalculateTotalService.calculateTotal(Iterable(Orange(), Orange(), Orange())) should be(0.50)
-
-    CalculateTotalService.calculateTotal(Iterable(Orange(), Orange(), Orange(), Orange())) should be(0.75)
-
-    CalculateTotalService.calculateTotal(Iterable(Orange(), Orange(), Orange(), Orange(), Orange())) should be(1.00)
-
-    CalculateTotalService.calculateTotal(Iterable(Orange(), Orange(), Orange(), Orange(), Orange(), Orange())) should be(1.00)
-  }
 }
